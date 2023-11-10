2ef77e2f2a50fd8e45df5512a238ca0c486e42a0
Hrishikesh <hrishikeshk19@gmail.com>
2023-11-10 19:13:07 +0530

    Initial commit

---
+++ app/src/main/java/com/hrishikesh/quizapp/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.hrishikesh.quizapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hrishikesh.quizapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
